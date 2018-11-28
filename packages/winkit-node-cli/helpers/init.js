d1e80459e20093ff21d4c3bbe3e22f4464cdb11f
Martin <martin.hloska@gmail.com>
2018-11-28 15:03:24 +0100

    basic init command

--- packages/winkit-node-cli/helpers/init.js
+++ packages/winkit-node-cli/helpers/init.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const {src, dest} = require('gulp');
+
 
 module.exports = function (cliArguments) {
+    const appDir = process.cwd();
+    const template_dir = __dirname +"/../templates";
 
-    console.log("initializing .... ");
-    console.log(cliArguments)
-
+    src(template_dir+"/*").pipe(dest(appDir)).on('error', (err) => {
+        console.log(err);
+    });
 };
