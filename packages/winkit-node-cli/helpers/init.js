d7a1bf2b53898109fd5c325b8aa2209ace151e9b
Martin <martin.hloska@gmail.com>
2018-11-29 09:13:06 +0100

    targets for init phase

--- packages/winkit-node-cli/helpers/init.js
+++ packages/winkit-node-cli/helpers/init.js
@@ -2,12 +2,18 @@
 
 const {src, dest} = require('gulp');
 
+const targets = {
+    '.env': {
+        template: 'env.template'
+    }
+}
+
 
 module.exports = function (cliArguments) {
     const appDir = process.cwd();
     const template_dir = __dirname +"/../templates";
 
-    src(template_dir+"/*").pipe(dest(appDir)).on('error', (err) => {
+    src(template_dir+"/*.js").pipe(dest(appDir)).on('error', (err) => {
         console.log(err);
     });
 };
